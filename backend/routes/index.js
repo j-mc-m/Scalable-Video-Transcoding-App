6e22831d370b8d92fa3a35ed7497b3528a6f4471
Jack Muir <jack.muir@connect.qut.edu.au>
2022-11-04 13:19:45 +1000

    Implemented status updates in DB Nothing crashed lmao Cleaned up some stuff

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,15 +1,15 @@
+// Imports
 var express = require('express');
 var router = express.Router();
 require('dotenv').config();
 var ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs').promises;
-//const md5 = require("md5");
 var path = require('path')
-
 
 // AWS configuration
 const AWS = require('aws-sdk');
 
+// Load AWS credentials from .env
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION,
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -17,33 +17,40 @@ AWS.config.update({
   sessionToken: process.env.AWS_SESSION_TOKEN
 });
 
-
+// Amazon DynamoDB setup
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const dynamoName = process.env.AWS_DYNAMO_DB_QUEUE;
 
-
-s3 = new AWS.S3({apiVersion: '2006-03-01'});
-
-// Buckets
-const s3Ingest = process.env.AWS_S3_INGEST;
-const s3Transcode = process.env.AWS_S3_TRANSCODE;
+// Load QUT username from .env file, DynamoDB & QUT IAM roles stuff
 const qut_username = process.env.QUT_USERNAME;
 
+// Amazon S3 setup
+s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
-const getS3KeyFromDynamo = async (dynamoID) => {
+// Load S3 bucket names from .env
+const s3Ingest = process.env.AWS_S3_INGEST;
+const s3Transcode = process.env.AWS_S3_TRANSCODE;
+
+
+
+
+const updateDynamo = async (dynamoID, status, s3TranscodeUrl) => {
+  const s3Key = await getS3KeyByDynamoID(dynamoID)
+  console.log("update s3 key: "  + s3Key + " did: " + dynamoID)
   const params = {
     TableName: dynamoName,
-    Key: {
+    Item: {
       'qut-username': qut_username,
       id: dynamoID,
-    }
+      s3Key: s3Key,
+      status: status,
+      s3TranscodeUrl: s3TranscodeUrl
+    },
   }
-  //return dynamoClient.get(params).promise();
-
-  const data = await dynamoClient.get(params).promise()
-  //console.log(data.Item.s3Url)
-  return data.Item.s3Key
+  return await dynamoClient.put(params).promise()
 }
+
+
 
 const getS3KeyByDynamoID = async (dynamoID) => {
   const params = {
@@ -54,29 +61,17 @@ const getS3KeyByDynamoID = async (dynamoID) => {
     }
   }
   //return dynamoClient.get(params).promise();
-
+  
   const data = await dynamoClient.get(params).promise()
-  //console.log(data.Item.s3Url)
   return data.Item.s3Key
 }
 
-const updateDynamo = async (id, status, s3TranscodeUrl) => {
-  const params = {
-    TableName: dynamoName,
-    Item: {
-      'qut-username': qut_username,
-      id: id,
-      status: status,
-      s3TranscodeUrl: s3TranscodeUrl
-    },
-  }
-  return await dynamoClient.put(params).promise()
-}
 
 
+downloadTmpFromS3 = async (dynamoID, s3Key) => {
+  const status = "pending download from S3"
+  updateDynamo(dynamoID, status, "")
 
-
-downloadTmpFromS3 = async (s3Key) => {
   const params = {
     Bucket: s3Ingest,
     Key: s3Key,
@@ -97,25 +92,12 @@ async function getData(s3Key) {
   return await s3.getObject(params)
 }
 
-/*
-const downloadTmpFromS3 = async(s3Key) => {
-  return new Promise((resolve, reject) => {
-    
-  
-
-    getData.then((data) => {
-      const location = '/tmp/' + s3Key
-      fs.writeFile(location, data)
-      resolve(location)
-    })
-  
-  })
-}*/
 
 
+async function uploadTranscodeToS3(dynamoID, file) {
+  const status = "uploading to public S3"
+  updateDynamo(dynamoID, status, "")
 
-
-async function uploadTranscodeToS3(file) {
   console.log("uploading " + file + " to s3 transcode bucket")
 
   const fileData = await fs.readFile(file)
@@ -134,10 +116,12 @@ async function uploadTranscodeToS3(file) {
         console.log(err)
         reject(err)
       } else {
-        console.log("successfully uploaded to public s3 transcode: " + result.Location)
-        //returnUrl = result.Location
-        // return result.Location
-        resolve(result.Location)
+        const s3TranscodeUrl = result.Location
+        console.log("successfully uploaded to public s3 transcode: " + s3TranscodeUrl)
+
+        const status = "finished"
+        updateDynamo(dynamoID, status, s3TranscodeUrl)
+        resolve(s3TranscodeUrl)
       }
     })
   })
@@ -145,9 +129,14 @@ async function uploadTranscodeToS3(file) {
 }
 
 
-async function transcode(file, s3Key) {
+async function transcode(dynamoID, file, s3Key) {
   const newFile = '/tmp/' + s3Key + '.mkv'
+
+  const status = "transcoding"
+  updateDynamo(dynamoID, status, "")
+
   console.log("ffmpeg file path: " + file)
+
   var ffmpegExec = await new ffmpeg(file)
 
   ffmpegExec.setFfmpegPath("/usr/bin/ffmpeg")
@@ -157,20 +146,26 @@ async function transcode(file, s3Key) {
       ffmpegExec.withSize('75%').withFps(24).toFormat('matroska')
         .on('end', function () {
           console.log('file has been converted successfully');
-            uploadTranscodeToS3(newFile).then(url => {
-              console.log("url:" + url)
-              resolve(url)
-            })
+
+          const status = "transcoded"
+          updateDynamo(dynamoID, status, "")
+
+          uploadTranscodeToS3(dynamoID, newFile).then(url => {
+            console.log("url:" + url)
+            resolve(url)
+          })
           /*return new uploadTranscodeToS3(newFile)/*.then(rsp => {
             console.log("rsp lmao: " + rsp)
           })*/
         })
         .on('error', function (err) {
             console.log('an error happened: ' + err.message);
+            reject(err.message)
         })
         .saveToFile(newFile)
     } catch(err) {
       console.log(err)
+      reject(err)
     }
   })
 }
@@ -200,11 +195,11 @@ router.post('/', async function(req, res) {
 
   try {
     const tmpFile = await new Promise((resolve, reject) => {
-      resolve(downloadTmpFromS3(s3Key)).catch((err) => reject(err))
+      resolve(downloadTmpFromS3(dynamoID, s3Key)).catch((err) => reject(err))
     })
 
     try {
-      transcode(tmpFile, s3Key).then(url => {
+      transcode(dynamoID, tmpFile, s3Key).then(url => {
       
         res.status(200).send({
           s3TranscodeUrl: url
@@ -221,15 +216,6 @@ router.post('/', async function(req, res) {
     console.log(err)
   }
 });
-
-
-
-
-
-
-
-
-
 
 
 module.exports = router;
